9a2013e1c024634ff67f016a41ab5af5ea5bba19
sagar <sagar>
2022-02-17 22:27:01 -0600

    unit test added

--- DiceRoller/app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ DiceRoller/app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -11,7 +11,9 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        val dice= MainActivity.Dice(8)
+        val rollResult=dice.roll()
+        assertTrue("The number was not between 1 and 6",rollResult in 1..6)
     }
 }
